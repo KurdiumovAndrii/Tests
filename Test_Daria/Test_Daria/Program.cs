2d9487cc3ba340a7a36d2dd3aa33cd85f4cd6522
Andrej <kurdiumov80@gmail.com>
2022-06-19 14:06:35 +0200

    Check alreadyAnswer

--- Test_Daria/Test_Daria/Program.cs
+++ Test_Daria/Test_Daria/Program.cs
@@ -14,12 +14,16 @@ namespace Test_Daria
         static int countArray = 0;
         static string[] readEveryLineDa = new string[1051];
         static string[] readEveryLineNet = new string[1081];
-      //  static int[] alreadyHave;
+        static int?[] alreadyHaveDa = new int?[50];
+        static int?[] alreadyHaveNet = new int?[50];
+        static int countDa = 0;
+        static int countNet = 0;
         static string userAnswer;
         static int correctCount = 0;
         static int countCheck = 1;
         static int questionType = 0;
         static int anotherCount = 1;
+        static bool check = false;
         static Random rand = new Random();
 
         static void Main(string[] args)
@@ -28,35 +32,37 @@ namespace Test_Daria
             int[] questionsDa = new int[210];
             int[] questionsNet = new int[181];
 
-            for(int i = 0; i<70; i++)
+            for (int i = 0; i < 70; i++)
             {
-                questionsDa[i] =+ count;
+                questionsDa[i] = +count;
                 count += 5;
             }
             count = 0;
             for (int l = 0; l < 81; l++)
             {
-                questionsNet[l] =+ count;
+                questionsNet[l] = +count;
                 count += 6;
             }
             count = 0;
- 
-                for (int k = 0; k < 51; k++)
-                {
-                    Generate(questionsDa, questionsNet);
+
+            for (int k = 0; k < 51; k++)
+            {
+                Generate(questionsDa, questionsNet);
+                countArray = 0;
+                userAnswer = null;
                 if (k == 50)
                 {
                     Console.Write(correctCount + "/" + countCheck++);
                     Console.Write("Умничка солнце!!");
                 }
- 
-                }
+
+            }
         }
         static void Generate(int[] questionsDa, int[] questionsNet)
         {
             //Console.OutputEncoding = System.Text.Encoding.UTF8;
             questionType = rand.Next(1, 3);
-            if(questionType == 1)
+            if (questionType == 1)
             {
                 GenerateDa(questionsDa);
             }
@@ -67,16 +73,23 @@ namespace Test_Daria
         }
         static void GenerateDa(int[] questionsDa)
         {
-            countArray = 0;
-            userAnswer = null;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            readEveryLineDa = File.ReadAllLines(fileDa, Encoding.UTF8);
+        q: readEveryLineDa = File.ReadAllLines(fileDa, Encoding.UTF8);
             countArray = questionsDa[rand.Next(0, questionsDa.Length)];
-            a: Console.ForegroundColor = ConsoleColor.Red;
+            for (int l = 0; l <= countDa + 1; l++)
+            {
+                if (alreadyHaveDa[l] == countArray)
+                {
+                    goto q;
+                }
+            }
+            alreadyHaveDa[countDa] = countArray;
+            countDa++;
+        a: Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(readEveryLineDa[countArray]);
             Console.ResetColor();
             Console.WriteLine();
-            for(int q = 1; q <= 3; q++)
+            for (int q = 1; q <= 3; q++)
             {
                 Console.WriteLine(readEveryLineDa[countArray + anotherCount]);
                 Console.WriteLine();
@@ -92,8 +105,9 @@ namespace Test_Daria
                 }
                 questionsDa = questionsDa.Where(val => val != countArray - 1).ToArray();
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(correctCount + "/" + countCheck++);
+                Console.WriteLine(correctCount + "/" + countCheck++);
                 Console.ResetColor();
+                Console.Write(readEveryLineDa[countArray + 4]);
                 Console.ReadKey();
                 Console.Clear();
             }
@@ -102,15 +116,26 @@ namespace Test_Daria
                 Console.WriteLine("Напиши А,Б или В :)");
                 goto a;
             }
+
+
         }
+
+
         static void GenerateNet(int[] questionsNet)
         {
-            countArray = 0;
-            userAnswer = null;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            readEveryLineNet = File.ReadAllLines(fileNet, Encoding.UTF8);
+        g: readEveryLineNet = File.ReadAllLines(fileNet, Encoding.UTF8);
             countArray = questionsNet[rand.Next(0, questionsNet.Length)];
-            b: Console.ForegroundColor = ConsoleColor.Red;
+            for (int l = 0; l <= countNet + 1; l++)
+            {
+                if (alreadyHaveNet[l] == countArray)
+                {
+                    goto g;
+                }
+            }
+            alreadyHaveNet[countNet] = countArray;
+            countNet++;
+        b: Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(readEveryLineNet[countArray]);
             Console.ResetColor();
             Console.WriteLine();
@@ -130,8 +155,9 @@ namespace Test_Daria
                 }
                 questionsNet = questionsNet.Where(val => val != countArray - 1).ToArray();
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(correctCount + "/" + countCheck++);
+                Console.WriteLine(correctCount + "/" + countCheck++);
                 Console.ResetColor();
+                Console.WriteLine(readEveryLineNet[countArray + 5]);
                 Console.ReadKey();
                 Console.Clear();
             }
@@ -143,3 +169,5 @@ namespace Test_Daria
         }
     }
 }
+
+
